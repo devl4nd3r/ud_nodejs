6f4e5f1dc54d18d53e9c6b92fbca9ea729764ecc
devl4nd3r <dev.l4nd3r@gmail.com>
2023-04-24 11:06:00 -0500

    removing duplicated files

--- dojo-js/template_strings.js
+++
@@ -1,7 +0,0 @@
-const superheroe = "Batman";
-const nombre = "Bruce Wayne";
-
-const concat = nombre + " alias:" + superheroe;
-const templateString = `${nombre} alias:${superheroe}`;
-console.log(concat);
-console.log(templateString);
